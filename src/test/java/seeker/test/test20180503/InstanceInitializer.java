5160a9caf5726d21ae253e6c407269421ada16aa
seeker <yanlei0419@163.com>
2018-07-11 18:02:17 +0800

    更新代码

---
+++ src/test/java/seeker/test/test20180503/InstanceInitializer.java
@@ -0,0 +1,10 @@
+package seeker.test.test20180503;
+
+public class InstanceInitializer {
+
+    private int i = 1;
+    {
+        j = i;  
+    }  
+    private int j;
+}  
--- src/test/java/seeker/test/test20180709/StringTest.java
+++ src/test/java/seeker/test/test20180709/StringTest.java
@@ -6,17 +6,18 @@ import java.util.Arrays;
 public class StringTest {
     public static void main(String[] args) throws Exception {
         String a="123456789";
-        String b=a.substring(3);
-        System.out.println(a);
-        System.out.println(b);
-        String val = "value";
-        Field fieldA = a.getClass().getDeclaredField(val);
-        fieldA.setAccessible(true);
-        Field fieldB = a.getClass().getDeclaredField(val);
-        fieldB.setAccessible(true);
-        char[] ca= (char[]) fieldA.get(a);
-        char[] cb= (char[]) fieldB.get(b);
-        System.out.println(Arrays.toString(ca));
-        System.out.println(Arrays.toString(cb));
+//        String b=a.substring(3);
+//        System.out.println(a);
+//        System.out.println(b);
+//        String val = "value";
+//        Field fieldA = a.getClass().getDeclaredField(val);
+//        fieldA.setAccessible(true);
+//        Field fieldB = a.getClass().getDeclaredField(val);
+//        fieldB.setAccessible(true);
+//        char[] ca= (char[]) fieldA.get(a);
+//        char[] cb= (char[]) fieldB.get(b);
+//        System.out.println(Arrays.toString(ca));
+//        System.out.println(Arrays.toString(cb));
+        a.wait();
     }
 }
